6eaef71037e31c8c48b90ae5b5fd2af15b80db27
SeungminKim <ksm0057@naver.com>
2020-08-31 21:16:11 +0900

    add button

--- src/App.js
+++ src/App.js
@@ -1,19 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import { Button } from "@material-ui/core";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  console.log(input);
+  console.log(messages);
+
+  const sendMessage = (event) => {
+    // all the logic to send a message goes
+    event.preventDefault();
+    setMessages([...messages, input]);
+    setInput("");
+  };
+
   return (
     <div className="App">
       <h1>Hello</h1>
-
-      <input />
-      <button>Send Message</button>
+      <form>
+        <input
+          value={input}
+          onChange={(event) => setInput(event.target.value)}
+        />
+        <Button
+          disabled={!input}
+          variant="contained"
+          color="primary"
+          type="submit"
+          onClick={sendMessage}
+        >
+          Send Message
+        </Button>
+      </form>
       {/* input field*/}
       {/* button */}
 
       {/* messages themselves */}
+      {messages.map((message) => (
+        <p>{message}</p>
+      ))}
     </div>
   );
 }
+
+/*
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="/__/firebase/7.19.1/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="/__/firebase/7.19.1/firebase-analytics.js"></script>
+
+<!-- Initialize Firebase -->
+<script src="/__/firebase/init.js"></script>
+ */
 
 export default App;
